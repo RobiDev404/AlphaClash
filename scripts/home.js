dd61e6b5e526fd32782e6e545e833d5ea51b4a20
Robi199 <mailrobi1998@gmail.com>
2024-09-03 23:24:53 +0600

    done

--- scripts/home.js
+++ scripts/home.js
@@ -4,24 +4,74 @@
 
 //     const playSection = document.getElementById('play-ground')
 //     playSection.classList.remove('hidden');
-    
+
 // }
 
 // function showPlayGround(){
-    
+
 // }
-function continueGame(){
+function addKeyboardKeyupHandler(event) {
+    const pressedKey = event.key;
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const alphabet = currentAlphabet.toLocaleLowerCase();
+    if (alphabet == pressedKey) {
+        console.log("you get a point");
+        removeBackgroundColorBiId(alphabet);
+        // increase score
+        const presentScore = updateScore('current-score');
+        const updatedScore = presentScore + 1;
+        setInnerTextValue('current-score', updatedScore);
+        continueGame();
+    }
+    else {
+        console.log('you press a wrong key');
+
+        const presentLife = updateScore('current-life');
+        const updatedLife = presentLife - 1;
+        setInnerTextValue('current-life', updatedLife);
+        if (presentLife == 0) {
+            removeHomeScreen('play-ground');
+            showPlayGround('result');
+            const presentScore = updateScore('current-score');
+
+            setInnerTextValue('total-score-board', presentScore);
+            toPlayAgain();
+            
+
+        }
+    }
+}
+document.addEventListener('keyup', addKeyboardKeyupHandler);
+
+
+
+function continueGame() {
+
+
     const alphabet = getRandomAlphabet();
     const currentAlphabet = document.getElementById('current-alphabet');
     currentAlphabet.innerText = alphabet;
-
-    addBackgroundColorBiId(alphabet);
     
+    addBackgroundColorBiId(alphabet);
+
 
 }
-function play(){
-    
+function play() {
+
     removeHomeScreen('home-screen');
+    removeHomeScreen('result');
     showPlayGround('play-ground');
+    setInnerTextValue('current-life', 5);
+    setInnerTextValue('current-score', 0);
+   
+
     continueGame();
+}
+function toPlayAgain(){
+const currentAlphabet = getElementTextById('current-alphabet');
+removeBackgroundColorBiId(currentAlphabet);
+    
+    
+    
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -14,6 +14,26 @@ function addBackgroundColorBiId(elementId){
     element.classList.add('bg-amber-400')
 }
 
+function removeBackgroundColorBiId(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-amber-400')
+   
+}
+function updateScore(elementId){
+    let element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const presentScore = parseFloat(elementText);
+    return presentScore;
+
+
+}
+function setInnerTextValue(elementId,value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+
+
 // creationg alphabet array
 function getRandomAlphabet(){
     const alphabetString = "abcdefghijklmnopqrstuvwxyz";
@@ -23,5 +43,11 @@ function getRandomAlphabet(){
     const index = Math.round(randomNumber);
     const alphabet = alphabetArray[index]
     return alphabet;
+    
+}
+function getElementTextById(elementId){
+    const elementText = document.getElementById(elementId);
+    const text = elementText.innerText;
+    return text;
     
 }
